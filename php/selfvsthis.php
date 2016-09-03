8a2f5914c4a4d012ebffe00ff8955a8308bae75d
Steven <the14thsky@gmail.com>
2016-09-03 12:39:52 +0800

    move file

--- php/selfvsthis.php
+++ php/selfvsthis.php
@@ -4,14 +4,17 @@ class ParentClass {
 		self::who();	// will output 'parent'
 		$this->who();	// will output 'child'
 	}
+
 	function who() {
 		echo 'parent';
 	}
 }
+
 class ChildClass extends ParentClass {
 	function who() {
 		echo 'child';
 	}
 }
+
 $obj = new ChildClass();
 $obj->test();
