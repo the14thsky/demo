c5a49f548f0efd39c24afbb82efe9780e970c2bc
Steven Lu <the14thsky@gmail.com>
2021-02-03 14:25:05 +0800

    added construction files

---
+++ construction/index.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>The14thsky</title>
+
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Megrim" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+    <style>
+        html {
+            -webkit-font-smoothing:antialiased;
+            text-rendering:optimizeLegibility;
+        }
+        body {
+            margin:0;
+            font-family:'Lato',sans-serif;
+        }
+        a, a:link, a:hover {
+            color:#444;
+            text-decoration:none;
+        }
+        h1 {
+            font-family:"Megrim",sans-serif;
+            letter-spacing:4px;
+            color:#444;
+        }
+        #initial {
+            display:block;
+            position:fixed;
+            background-color:#fff;
+            padding-top:15%;
+            height:100%;
+            width:100%;
+            z-index:99;
+            top:0;
+            text-align:center;
+        }
+        #initial h1 {
+            margin:0;
+            font-size:60px;
+            font-weight:300;
+        }
+        #initial .subtitle {
+            margin-top: -5px;
+            font-size: 16px;
+            font-weight: 100;
+            letter-spacing: 3px;
+            text-transform: lowercase;
+        }
+        .contact {
+            margin:30px auto 0;
+            width:100%;
+            max-width:700px;
+            border-top:1px solid #eee;
+        }
+        .social-media {
+            margin-top:30px;
+        }
+        .social-media a {
+            margin:0 10px;
+        }
+        .social-media a i {
+            font-size:30px;
+            color:#666;
+        }
+        .social-media a i:hover {
+            color:#333;
+        }
+        @media (max-width:575.98px){
+            #initial h1 {
+                font-size:44px;
+            }
+        }
+    </style>
+</head>
+<body>
+<section id="initial">
+    <div class="container text-center">
+        <h1>the14thsky</h1>
+        <p class="subtitle">Web Developer</p>
+        <div class="contact">
+            <div class="social-media mb-2">
+                <a href="http://www.linkedin.com/in/the14thsky" target="_blank"><i class="fab fa-linkedin"></i></a>
+                <a href="https://github.com/the14thsky" target="_blank"><i class="fab fa-github-square"></i></a>
+                <a href="https://twitter.com/the14thsky" target="_blank"><i class="fab fa-twitter-square"></i></a>
+                <a href="https://the14thsky.tumblr.com" target="_blank"><i class="fab fa-tumblr-square"></i></a>
+            </div>
+        </div>
+    </div>
+</section>
+</body>
+</html>
